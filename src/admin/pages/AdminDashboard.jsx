6ed9a36f2c022bd96a00fb62d4377101b25086bb
ayusha1223 <ayushathapa425@gmail.com>
2026-07-04 10:47:21 +0545

    Admin Dashboard Added

---
+++ src/admin/pages/AdminDashboard.jsx
@@ -0,0 +1,31 @@
+import AdminSidebar from "../components/AdminSidebar";
+
+export default function AdminDashboard() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex">
+
+      {/* Sidebar */}
+
+      <AdminSidebar />
+
+      {/* Main */}
+
+      <div className="flex-1 ml-72">
+
+        <div className="p-10">
+
+          <h1 className="text-4xl font-bold text-[#0F4C81]">
+            Admin Dashboard
+          </h1>
+
+          <p className="text-gray-500 mt-2">
+            Welcome to the TrippyGo Administration Panel.
+          </p>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
