ee025a36cc45d0eb9f1b257354a4139aeb2fee8a
Ayusha Thapa <ayushathapa@Ayushas-MacBook-Air-6.local>
2026-09-03 07:31:28 +0545

    Error saved

--- src/landing/pages/LandingPage.tsx
+++ src/landing/pages/LandingPage.tsx
@@ -1,15 +1,14 @@
 import NavBar from "../components/NavBar";
-import Hero from "../components/Hero";
 import Stats from "../components/Stats";
 import Destinations from "../components/Destinations";
 import Adventures from "../components/Adventures";
 import Footer from "../components/Footer";
 
+
 function LandingPage() {
 return (
-<> <NavBar /> <Hero /> <Stats /> <Destinations /> <Adventures /> <Footer />
+<> <NavBar /> <Stats /> <Destinations /> <Adventures /> <Footer />
 </>
 );
 }
-
 export default LandingPage;
