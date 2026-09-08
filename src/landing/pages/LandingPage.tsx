d6016ec2c8224d9da964c652ca5cf21fd12ef129
Ayusha Thapa <ayushathapa@Ayushas-MacBook-Air-6.local>
2026-09-08 12:07:27 +0545

    deleted navbar

--- src/landing/pages/LandingPage.tsx
+++ src/landing/pages/LandingPage.tsx
@@ -1,15 +1,12 @@
-import NavBar from "../components/NavBar";
 import Stats from "../components/Stats";
 import Destinations from "../components/Destinations";
 import Adventures from "../components/Adventures";
 import Footer from "../components/Footer";
 import InternalNavBar from "../components/InternalNavbar";
 
-
-
 function LandingPage() {
 return (
-<> <NavBar /> <InternalNavBar /> <Stats /> <Destinations /> <Adventures /> <Footer />
+<> <InternalNavBar /> <Stats /> <Destinations /> <Adventures /> <Footer />
 </>
 );
 }
